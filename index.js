3d352e90f108fc0c8b6a6c99e07edccda191dd7f
April <a.d.cidoni@gmail.com>
2023-11-26 14:07:00 -0500

    phase-1-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination, distanceInBlocks){
+    if (destination >= 42){
+    return distanceInBlocks = (destination - 42);
+     } else if (destination <= 42) {
+        return distanceInBlocks = (42 - destination)
+     }
+ }
+function distanceFromHqInFeet(destination, distanceInFeet){
+    if (destination >= 42){
+        return distanceInFeet = ((destination - 42) * 264)
+    }else if (destination <= 42){
+        return distanceInFeet = ((42 - destination) * 264)
+    }
+
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination){
+    return ((start - destination)*264)
+    }else if (start <= destination){
+        return ((destination - start)*264)
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination)
+    if(distance <= 400){
+        return (0)
+    }else if (distance >= 400 && distance <= 2000){
+        return (distance-400)*0.02;
+    }else if (distance >= 2000 && distance <= 2500){
+        return 25;
+    }else if (distance >= 2500){
+        return 'cannot travel that far'
+    }
+
+}
